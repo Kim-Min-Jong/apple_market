e6de389e9aed1b64a96befe4352ef428dfb93d62
minjong kim <alswhddkdl@gmail.com>
2023-08-22 19:47:47 +0900

    [FEAT] : ⚙️ RecyclerView 어탭터 생성 및 연결

--- app/src/main/java/com/sparta/applemarket/MainActivity.kt
+++ app/src/main/java/com/sparta/applemarket/MainActivity.kt
@@ -2,10 +2,25 @@ package com.sparta.applemarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sparta.applemarket.adapter.ProductAdapter
+import com.sparta.applemarket.data.ProductsData
+import com.sparta.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() = with(binding) {
+        val list = ProductsData(this@MainActivity).getList()
+        mainRecyclerView.adapter = ProductAdapter(this@MainActivity).apply {
+            addItems(list)
+        }
+        mainRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
---
+++ app/src/main/java/com/sparta/applemarket/adapter/ProductAdapter.kt
@@ -0,0 +1,50 @@
+package com.sparta.applemarket.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sparta.applemarket.R
+import com.sparta.applemarket.databinding.ItemSaleProductBinding
+import com.sparta.applemarket.model.Product
+
+class ProductAdapter(private val context: Context) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+    private val datas = arrayListOf<Product>()
+
+    fun addItems(items: List<Product>) {
+        datas.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            ItemSaleProductBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int = datas.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(datas[position])
+
+    inner class ViewHolder(
+        private val binding: ItemSaleProductBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(model: Product) {
+            with(binding) {
+                mainImageView.run {
+                    setImageResource(model.image)
+                    clipToOutline = true
+                }
+                itemTitle.text = model.productName
+                itemAddress.text = model.address
+                "${model.price}${context.getString(R.string.won)}".also { itemPrice.text = it }
+                itemChat.text = "${model.chatting}"
+                itemLiked.text = "${model.liked}"
+            }
+        }
+    }
+}
