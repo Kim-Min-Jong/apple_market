5d7322e8fcad6d15c5bdff5f6980bf36596c1e5b
minjong kim <alswhddkdl@gmail.com>
2023-08-22 21:00:29 +0900

    [FEAT] : ⚙️ 상단 버튼 누르면 Notification 발생하는 기능 구현 (안드로이드 13 이상 대응 포함)

--- app/src/main/java/com/sparta/applemarket/MainActivity.kt
+++ app/src/main/java/com/sparta/applemarket/MainActivity.kt
@@ -3,12 +3,14 @@ package com.sparta.applemarket
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sparta.applemarket.adapter.ProductAdapter
 import com.sparta.applemarket.data.ProductsData
 import com.sparta.applemarket.databinding.ActivityMainBinding
+import com.sparta.applemarket.notification.ProductNotification
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,8 +24,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRecyclerView()
+        initButton()
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
             this.onBackPressedDispatcher.addCallback(callback)
+    }
+    private fun initButton() {
+        binding.notificationButton.setOnClickListener {
+            Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show()
+            ProductNotification(this@MainActivity).runNotification()
+        }
     }
 
     private fun initRecyclerView() = with(binding) {
---
+++ app/src/main/java/com/sparta/applemarket/notification/ProductNotification.kt
@@ -0,0 +1,72 @@
+package com.sparta.applemarket.notification
+
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.sparta.applemarket.MainActivity
+import com.sparta.applemarket.R
+
+class ProductNotification(private val context: Context) {
+    private val myNotificationID = 1
+    fun runNotification() {
+        createNotificationChannel()
+        val notification = createNotification()
+        // 안드로이드 13이후로는 notification 을 하려면 권한 설정이 필요함
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                context as MainActivity,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                1
+            )
+        }
+        NotificationManagerCompat.from(context).notify(myNotificationID, notification)
+    }
+
+    private fun createNotification(): Notification {
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        builder.apply {
+            setSmallIcon(R.drawable.notifications_clicked)
+            setContentTitle(context.getString(R.string.notification_title))
+            setContentText(context.getString(R.string.notification_text))
+        }
+
+        return builder.build()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // Android 8.0
+            val channel = NotificationChannel(
+                CHANNEL_ID, "apple market channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "this is apple market's channel"
+                val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttributes)
+                enableVibration(true)
+            }
+            (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "APPLE_MARKET"
+    }
+}
